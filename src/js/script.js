3d66aec74488d9315d7f406fd1c0ebfb4113f47f
Anton27 <antonkleymenov27@yandex.ru>
2020-08-07 21:54:34 +0300

    delete classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
+    $('.burger').click(function (event) {
+        $('.burger,.menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
 
